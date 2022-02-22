0319f36b2b167acf511c3a704db5a7af98cf5394
Paola Miranda <88098114+pamiranda@users.noreply.github.com>
2022-02-21 22:04:52 -0300

    Add files via upload

---
+++ Desafio1.cs
@@ -0,0 +1,16 @@
+//Minha solução
+
+string projectName = "ACME";
+string russianMessage = "\u041f\u043e\u0441\u043c\u043e\u0442\u0440\u0435\u0442\u044c \u0440\u0443\u0441\u0441\u043a\u0438\u0439 \u0432\u044b\u0432\u043e\u0434";
+Console.WriteLine($"View English output:\n\t\tc:\\Exercise\\{projectName}\\data.txt");
+Console.WriteLine($"\n{russianMessage}:\n\t\tc:\\Exercise\\{projectName}\\ru-RU\\data.txt");
+
+//Solução Correta
+
+string projectName = "ACME";
+string englishLocation = $@"c:\Exercise\{projectName}\data.txt";
+Console.WriteLine($"View English output:\n\t\t{englishLocation}\n");
+            
+string russianMessage = "\u041f\u043e\u0441\u043c\u043e\u0442\u0440\u0435\u0442\u044c \u0440\u0443\u0441\u0441\u043a\u0438\u0439 \u0432\u044b\u0432\u043e\u0434";
+string russianLocation = $@"c:\Exercise\{projectName}\ru-RU\data.txt";
+Console.WriteLine($"{russianMessage}:\n\t\t{russianLocation}\n");
