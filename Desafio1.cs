2a036477cfd33aa50f415caa061fbabd6c5156fd
Paola Miranda <88098114+pamiranda@users.noreply.github.com>
2022-02-22 10:31:49 -0300

    Update Desafio1.cs
    Análise

--- Desafio1.cs
+++ Desafio1.cs
@@ -4,6 +4,8 @@ string projectName = "ACME";
 string russianMessage = "\u041f\u043e\u0441\u043c\u043e\u0442\u0440\u0435\u0442\u044c \u0440\u0443\u0441\u0441\u043a\u0438\u0439 \u0432\u044b\u0432\u043e\u0434";
 Console.WriteLine($"View English output:\n\t\tc:\\Exercise\\{projectName}\\data.txt");
 Console.WriteLine($"\n{russianMessage}:\n\t\tc:\\Exercise\\{projectName}\\ru-RU\\data.txt");
+//***Funcionou mas não devia ter usado barra dupla
+
 
 //Solução Correta
 
