613a02a86a5f3294cc8758cf49c5b502a5a37e93
Paola Miranda <88098114+pamiranda@users.noreply.github.com>
2022-02-25 10:10:21 -0300

    Add files via upload

---
+++ Desafio 3.cs
@@ -0,0 +1,16 @@
+
+//eu trabalho é pesquisar e preencher a linha de código ausente que chama um método da classe Math que aceitará dois valores e retornará o maior deles na variável largerValue, que será impressa no console.
+
+//Minha solução:
+int firstValue = 500;
+int secondValue = 600;
+int largeValue = Math.Max(firstValue, secondValue);
+Console.WriteLine(largeValue);
+
+//Solução do curso:
+
+int firstValue = 500;
+int secondValue = 600;
+int largerValue;
+largerValue = Math.Max(firstValue, secondValue);
+Console.WriteLine(largerValue);
